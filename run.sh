42ec72e5d131fe1946dd6a44a9dff4c31bec416e
NomikLee <Nomiklee.icloud.com>
2025-03-04 14:03:06 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /Users/nomiklee/PycharmProjects/PythonProject/PythonlearnTest
+source /Users/nomiklee/PycharmProjects/PythonProject/PythonlearnTest/.venv/bin/activate
+pytest -v -s --html reports/report.html
